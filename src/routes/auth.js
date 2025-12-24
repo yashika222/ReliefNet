3f4f09409485dec3246e4de472fafe9a557e0d56
yashika <yashika2222255@gmail.com>
2025-12-24 09:22:13 +0530

    restore homepage route after stable deploy

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,8 +8,6 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 
 // Web routes for login/signup pages
